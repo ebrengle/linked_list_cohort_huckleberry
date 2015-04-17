5cb386de612567b3ec62a610d72cac33e9b9f1b6
Eric Brengle <eric.brengle@gmail.com>
2015-04-17 09:49:46 -0500

    did another example of the push function

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,17 +1,21 @@
+require_relative 'linked_list_item'
+
 class LinkedList
 
+  def initialize
+    @indexCounter = -1
+  end
+
   def get(nth_item)
+    raise IndexError if nth_item < 0
     if nth_item == 0
       @first_item.payload
-    elsif nth_item < 0
-      raise IndexError
     else
       count = 0
       current_node = @first_item
       while count < nth_item
-        if current_node.next_item == nil
-          raise IndexError
-        end
+        raise IndexError if current_node.nil?
+
         current_node = current_node.next_item
         count = count + 1
       end
@@ -20,6 +24,7 @@ class LinkedList
   end
 
   def push(item)
+    @indexCounter += 1
    if  @first_item.nil?
      @first_item = LinkedListItem.new(item)
      @last_item = @first_item
@@ -28,6 +33,24 @@ class LinkedList
      @last_item = @last_item.next_item
    end
 
+  end
+
+
+  #Eliza's Push Method Demo
+  #def push(payload)
+    #if @first_item.nil?
+    #@first_item = LinkedListItiem.new(payload)
+    #else
+      #last_item = @first_item
+      #until last_item.last?
+        #last_item = last_item.next_item
+      #end
+      #last_item.next_item = new.item
+    #end
+  #end
+
+  def size
+    @indexCounter += 1
   end
 
 end
